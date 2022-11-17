7c8965dc33fcf9a7846e246777f8ce320cdeda22
Lee David <leedav87@gmail.com>
2022-11-17 10:51:21 -0300

    dimensions react native

--- 02-MyFirstApp/App.tsx
+++ 02-MyFirstApp/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import { ContadorScreen } from './src/screens/ContadorScreen';
-import { HolaMundoScreen } from './src/screens/HolaMundoScreen';
+import { SafeAreaView } from 'react-native';
+import { BoxObjectModelScreen } from './src/screens/BoxObjectModelScreen';
+import { DimensionesScreen } from './src/screens/DimensionesScreen';
+// import { ContadorScreen } from './src/screens/ContadorScreen';
+// import { HolaMundoScreen } from './src/screens/HolaMundoScreen';
 
 const App = () => {
     return (
-
-        // <HolaMundoScreen />
-        <ContadorScreen />
+        <SafeAreaView style={{ flex: 1 }}>
+            {/* <HolaMundoScreen /> */}
+            {/* <ContadorScreen /> */}
+            {/* <BoxObjectModelScreen /> */}
+            <DimensionesScreen />
+        </SafeAreaView>
 
     )
 };
