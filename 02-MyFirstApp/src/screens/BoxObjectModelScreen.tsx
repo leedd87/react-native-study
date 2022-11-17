52f45fbc2a2989154cf5534509a09cfdc70c8021
Lee David <leedav87@gmail.com>
2022-11-17 10:50:55 -0300

    box modeling react native

---
+++ 02-MyFirstApp/src/screens/BoxObjectModelScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export const BoxObjectModelScreen = () => {
+    return (
+        <View><Text>
+            BoxObjectModel
+        </Text>
+        </View>
+    )
+}
