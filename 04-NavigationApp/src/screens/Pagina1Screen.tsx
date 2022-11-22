75828e6d724f63c2fdb690e083b2cdecd2801659
Lee David <leedav87@gmail.com>
2022-11-22 12:02:17 -0300

    add stack navigator react native tsx

---
+++ 04-NavigationApp/src/screens/Pagina1Screen.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export const Pagina1Screen = () => {
+    return (
+        <View>
+            <Text>
+                Pagina1Screen
+            </Text>
+        </View>
+    )
+}
---
+++ 04-NavigationApp/src/screens/Pagina3Screen.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export const Pagina3Screen = () => {
+    return (
+        <View>
+            <Text>
+                Pagina3Screen
+            </Text>
+        </View>
+    )
+}
