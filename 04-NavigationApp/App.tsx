fd5d5c12fbb23923e48db753fe749b7ce1dc6bdd
Lee David <leedav87@gmail.com>
2022-11-24 10:53:27 -0300

    drawer tsx

--- 04-NavigationApp/App.tsx
+++ 04-NavigationApp/App.tsx
@@ -1,12 +1,17 @@
+//import 'react-native-gesture-handler';
 import React from 'react'
-import { Text, View } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
 import { StackNavigator } from './src/navigator/StackNavigator';
+import { MenuLateralBasico } from './src/navigator/MenuLateralBasico';
+
 
 const App = () => {
   return (
+
+
     <NavigationContainer>
       <StackNavigator />
+      {/* <MenuLateralBasico /> */}
     </NavigationContainer>
   )
 }
