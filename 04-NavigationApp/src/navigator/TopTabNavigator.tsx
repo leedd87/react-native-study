8efa4fcaad69b88a769d559c41a9c839f3bb6262
Lee David <leedav87@gmail.com>
2022-11-30 13:04:02 -0300

    top tab navigator

--- 04-NavigationApp/src/navigator/TopTabNavigator.tsx
+++ 04-NavigationApp/src/navigator/TopTabNavigator.tsx
@@ -3,12 +3,23 @@ import { createMaterialTopTabNavigator } from '@react-navigation/material-top-ta
 import { ChatScreen } from '../screens/ChatScreen';
 import { ContactsScreen } from '../screens/ContactsScreen';
 import { AlbumsScreen } from '../screens/AlbumsScreen';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { colores } from '../themes/appTheme';
 
 const Tab = createMaterialTopTabNavigator();
 
 export const TopTabNavigator = () => {
+
+    const { top } = useSafeAreaInsets()
+
     return (
-        <Tab.Navigator>
+        <Tab.Navigator
+            style={{ paddingTop: top }}
+            sceneContainerStyle={{ backgroundColor: 'white' }}
+            screenOptions={({ route }) => ({
+                tabBarPressColor: colores.primary
+            })}
+        >
             <Tab.Screen name="ChatScreen" component={ChatScreen} />
             <Tab.Screen name="ContactsScreen" component={ContactsScreen} />
             <Tab.Screen name="AlbumsScreen" component={AlbumsScreen} />
